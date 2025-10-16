4ef7e0b90772edb9d94ba3894b41155e511d9e8e
Johncarlzzz <ogayonjohncarl23@gmail.com>
2025-10-16 19:42:18 +0800

    first commit

---
+++ tanggol.py
@@ -0,0 +1,6 @@
+# hello_poof.py
+print("Hello, World!")
+print()  # blank line
+# Poop bomb explosion (one-shot)
+print("💣 BOOM! 💥  Poop bomb exploded:")
+print("💩 " * 30)  # prints 30 poop emojis in a row
